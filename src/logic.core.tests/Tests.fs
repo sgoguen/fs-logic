a4cba83a8e12cb10aec7ead864ef231d6de0da6b
Steve Goguen <sgoguen@gmail.com>
2025-04-13 14:56:14 -0400

    Updated to .NET Core

---
+++ src/logic.core.tests/Tests.fs
@@ -0,0 +1,94 @@
+﻿module Tests
+
+open System
+open Xunit
+open logic.core.Logic
+
+module Utils = 
+    let inline assertEqual (expected: 'a) (actual: 'a) =
+        Assert.Equal<'a>(expected, actual)
+
+open Utils
+
+[<Fact>]
+let ``Basic`` () =
+    let example1 q = 
+        logic {
+            let! y = fresh
+            do! y == Int 42
+            do! q == y
+        }
+
+    let result = run 1 example1
+    result |> assertEqual [ Int 42 ]
+
+
+    let example2 q = 
+        logic {
+            let! x = fresh
+            let! y = fresh
+            do! x == Int 42
+            do! y == Int 24
+            do! q == x
+            do! q == y
+        }
+
+    run 1 example2 |> assertEqual [ ]
+
+    let example3 q = 
+        logic {
+            let! x = fresh
+            let! y = fresh
+            do! x == Int 42
+            do! y == Int 24
+            do! conde [q == x; q == y]
+        }
+
+    run 2 example3 |> assertEqual [ Int 42; Int 24 ]
+
+    let rec fives x = 
+        logic {
+            do! conde [x == Int 5;
+                    logic { return! fives x }]
+        }
+
+    run 9 fives |> assertEqual [ Int 5; Int 5; Int 5; Int 5; Int 5; Int 5; Int 5; Int 5; Int 5 ]
+
+    let rec sixes x = 
+        logic {
+            do! conde [x == Int 6;
+                    logic { return! sixes x }]
+        }
+
+    run 9 (fun q -> conde [fives q; sixes q])
+    |> assertEqual [ Int 5; Int 6; Int 5; Int 6; Int 5; Int 6; Int 5; Int 6; Int 5 ]
+
+    let rec peano n =
+        logic {
+            do! conde [ Str "z" == n;
+                        logic {
+                            let! n' = fresh
+                            do! Pair (Str "s", n') == n
+                            return! peano n'
+                        } ]
+        }
+
+    run 3 (fun q -> peano q)
+    |> assertEqual [Str "z"; Pair (Str "s", Str "z"); Pair (Str "s", Pair (Str "s", Str "z"))]
+
+
+    let rec appendo l s out =
+        logic {
+            do! conde [ logic { do! l == Empty
+                                do! s == out };
+                        logic {
+                            let! a, d, res = fresh3 
+                            do! Pair (a, d) == l
+                            do! Pair (a, res) == out
+                            return! appendo d s res
+                        }]
+        }
+
+    run 2 (fun x -> appendo x (Pair (Int 3, Empty)) (Pair (Int 1, Pair (Int 2, Pair (Int 3, Empty)))))
+    |> assertEqual [Pair (Int 1, Pair (Int 2, Empty))]
+
--- src/logic.core/logic.fs
+++ src/logic.core/logic.fs
@@ -1,16 +1,18 @@
 ﻿namespace logic.core
+
 open System
 open System.Collections.Generic
 open System.Linq
 
-module Logic = 
+module Logic =
 
     // Type defs
     type VarName = string
-    type Term = 
-        | Var of VarName 
-        | Pair of Term * Term 
-        | Int of int 
+
+    type Term =
+        | Var of VarName
+        | Pair of Term * Term
+        | Int of int
         | Str of string
         | Empty
 
@@ -22,9 +24,9 @@ module Logic =
     // walk : Term -> Subst -> Term
     let rec walk term subst =
         match term with
-        | Var var -> 
+        | Var var ->
             match List.tryFind (fun (var', _) -> var = var') subst with
-            | Some (_, term') -> walk term' subst
+            | Some(_, term') -> walk term' subst
             | None -> Var var
         | _ -> term
 
@@ -33,36 +35,38 @@ module Logic =
     // unify : Term -> Term -> Subst -> Subst option
     let rec unify term term' subst =
         match walk term subst, walk term' subst with
-        | Var var, Var var' -> 
-            if var = var' then 
-                Some subst 
-            else 
-                Some <| extend subst (var, Var var') 
+        | Var var, Var var' ->
+            if var = var' then
+                Some subst
+            else
+                Some <| extend subst (var, Var var')
         | Var var, _ -> Some <| extend subst (var, term')
         | _, Var var -> Some <| extend subst (var, term)
-        | Pair (leftTerm, rightTerm), Pair (leftTerm', rightTerm') ->
+        | Pair(leftTerm, rightTerm), Pair(leftTerm', rightTerm') ->
             match unify leftTerm leftTerm' subst with
             | Some subst' -> unify rightTerm rightTerm' subst'
             | None -> None
         | Int v, Int v' -> if v = v' then Some subst else None
-        | Str v, Str v' -> if v = v' then Some subst else None 
-        | Empty, Empty -> Some subst 
+        | Str v, Str v' -> if v = v' then Some subst else None
+        | Empty, Empty -> Some subst
         | _, _ -> None
 
 
-    // disj : Goal<'T> -> Goal<'T> -> Goal<'T> 
-    let disj (goal : Goal<'T>) (goal' : Goal<'T>) =
+    // disj : Goal<'T> -> Goal<'T> -> Goal<'T>
+    let disj (goal: Goal<'T>) (goal': Goal<'T>) =
         fun (state, counter) ->
-            let rec interleave (enum : Lazy<IEnumerator<_>>) (enum' : Lazy<IEnumerator<_>>) = 
-                seq { 
+            let rec interleave (enum: Lazy<IEnumerator<_>>) (enum': Lazy<IEnumerator<_>>) =
+                seq {
                     let flag = enum.Force().MoveNext()
+
                     if flag then
                         yield enum.Force().Current
-                    
+
                     let flag' = enum'.Force().MoveNext()
+
                     if flag' then
                         yield enum'.Force().Current
-                    
+
                     if flag || flag' then
                         yield! interleave enum enum'
                 }
@@ -75,66 +79,74 @@ module Logic =
     let conde list = List.reduce disj list
 
     // (==) : Term -> Term -> Goal<unit>
-    let (==) (term : Term) (term' : Term) : Goal<unit> = 
+    let (==) (term: Term) (term': Term) : Goal<unit> =
         fun (subst, counter) ->
             match unify term term' subst with
             | Some subst' -> seq { yield (subst', counter), () }
             | None -> Seq.empty
 
     // fresh : Goal<Term>
-    let fresh : Goal<Term> = fun (subst, counter) -> seq { yield ((subst, counter + 1), (Var (sprintf "var%d" counter))) }
+    let fresh: Goal<Term> =
+        fun (subst, counter) -> seq { yield ((subst, counter + 1), (Var(sprintf "var%d" counter))) }
     // fresh2 : Goal<Term * Term>
-    let fresh2 : Goal<Term * Term> = fun (subst, counter) -> seq { yield ((subst, counter + 2), (Var (sprintf "var%d" counter), Var (sprintf "var%d" (counter + 1)))) }
+    let fresh2: Goal<Term * Term> =
+        fun (subst, counter) ->
+            seq { yield ((subst, counter + 2), (Var(sprintf "var%d" counter), Var(sprintf "var%d" (counter + 1)))) }
     // fresh3 : Goal<Term * Term * Term>
-    let fresh3 : Goal<Term * Term * Term> = fun (subst, counter) -> seq { yield ((subst, counter + 3), (Var (sprintf "var%d" counter), Var (sprintf "var%d" (counter + 1)), Var (sprintf "var%d" (counter + 2)))) }
+    let fresh3: Goal<Term * Term * Term> =
+        fun (subst, counter) ->
+            seq {
+                yield
+                    ((subst, counter + 3),
+                     (Var(sprintf "var%d" counter),
+                      Var(sprintf "var%d" (counter + 1)),
+                      Var(sprintf "var%d" (counter + 2))))
+            }
 
     // substVars : Subst -> Term -> Term
-    let rec substVars subst term = 
-            match term with
-            | Var _ -> 
-                let term' = walk term subst
-                if term = term' then term
-                else substVars subst term'
-            | Pair (left, right) -> Pair (substVars subst left, substVars subst right)
-            | Int _ -> term
-            | Str _ -> term
-            | Empty -> term  
+    let rec substVars subst term =
+        match term with
+        | Var _ ->
+            let term' = walk term subst
+            if term = term' then term else substVars subst term'
+        | Pair(left, right) -> Pair(substVars subst left, substVars subst right)
+        | Int _ -> term
+        | Str _ -> term
+        | Empty -> term
 
     // print : string -> Term -> Goal<unit>
-    let print name (term : Term) = 
-         fun (subst, counter) -> 
-              printfn "%s = %A" name (substVars subst (walk term subst))
-              seq { yield ((subst, counter), ()) } 
+    let print name (term: Term) =
+        fun (subst, counter) ->
+            printfn "%s = %A" name (substVars subst (walk term subst))
+            seq { yield ((subst, counter), ()) }
 
     // run : int -> (Term -> Goal<'T>) -> Term list
-    let run n (f : Term -> Goal<'T>) = 
+    let run n (f: Term -> Goal<'T>) =
         let goalVar = Var "__goal__"
-        let seq = 
-            ([], 0)
-            |> f goalVar
-        seq.Take(n)
+        let seq = ([], 0) |> f goalVar
+
+        seq
+        |> Seq.truncate n
         |> Seq.map (fun ((subst, _), _) -> substVars subst (walk goalVar subst))
         |> Seq.toList
 
 
     type LogicBuilder() =
 
-        member this.Zero() = fun _ -> Seq.empty 
-        member this.Return v = fun (subst, counter) -> seq { yield ((subst, counter), v) } 
-        member this.ReturnFrom goal = goal 
-    
-        member this.Bind (goal : Goal<'T>, f : 'T -> Goal<'R>) : Goal<'R> =
-            fun (subst, counter) -> 
+        member this.Zero() = fun _ -> Seq.empty
+
+        member this.Return v =
+            fun (subst, counter) -> seq { yield ((subst, counter), v) }
+
+        member this.ReturnFrom goal = goal
+
+        member this.Bind(goal: Goal<'T>, f: 'T -> Goal<'R>) : Goal<'R> =
+            fun (subst, counter) ->
                 (subst, counter)
                 |> goal
-                |> Seq.collect (fun ((subst', counter'), v) -> 
-                                    f v (subst', counter'))  
+                |> Seq.collect (fun ((subst', counter'), v) -> f v (subst', counter'))
 
-        member this.Delay (f : unit -> Goal<'T>) : Goal<'T> =
-            fun (subst, counter) ->
-                f () (subst, counter) 
-        
+        member this.Delay(f: unit -> Goal<'T>) : Goal<'T> =
+            fun (subst, counter) -> f () (subst, counter)
+
     let logic = new LogicBuilder()
-
-
-    
